5fc69f030b16ed453cc7039e56911121ba5bc561
Rustam Eynaliyev <rustam.eynaliyev@gmail.com>
2017-10-09 15:35:25 +0200

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {DataListModule} from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
+import { MoviesListComponent } from './movies-list/movies-list.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { HeaderComponent } from './app-header/app-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesListComponent,
+    MovieDetailComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DataListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
